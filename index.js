ee5334bf0fdeb23164d19b3674bf8027e820c117
Hyun Sung Cho <hcho774@gmail.com>
2022-04-05 13:33:03 -0400

    commit tree d9747a5ad453db17ee0613ad6b5decc10bac5a6a

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    let newArr = cats.slice();
+    newArr.push("Broom");
+    return newArr;
+}
+
+function prependCat(name) {
+    let newArr = cats.slice();
+    newArr.unshift("Arnold");
+    return newArr;
+}
+
+function removeLastCat() {
+    let newArr = cats.slice();
+    newArr.pop();
+    return newArr;
+}
+
+function removeFirstCat() {
+    let newArr = cats.slice();
+    newArr.shift();
+    return newArr;
+}
